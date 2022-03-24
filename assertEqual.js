83ab59764c2ecac8f297bf21269bd704b7e4c6a9
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-23 23:53:34 -0700

    Improved by using template literals. Assigned variables for both pass and fail messages.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,27 @@
-// FUNCTION IMPLEMENTATION HERE:
-const assertEqual = function(actual, expected) {
+// FUNCTION IMPLEMENTATION HERE
+
+/* (Mod1,Wk1: assertEqual > Implement assertEqual):
+Original code with concatenated strings.
+
+const assertEqual = function (actual, expected) {
+  const pass = "";
   if (actual === expected) {
     console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
   } else {
     console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};*/
+
+/* (Mod1,Wk1: assertEqual > Improve assertEqual):
+Updated code that uses template literals. */
+
+const assertEqual = function(actual, expected) {
+  const pass = "✅✅✅ Assertion Passed: ";
+  const fail = "🛑🛑🛑 Assertion Failed: ";
+  if (actual === expected) {
+    console.log(`${pass}` + actual + " === " + expected);
+  } else {
+    console.log(`${fail}` + actual + " !== " + expected);
   }
 };
 
@@ -13,3 +31,5 @@ assertEqual(1,1);
 // Testing scenarios of non-identical values:
 assertEqual("Weekend", "Weekend");
 assertEqual(1,5);
+
+
