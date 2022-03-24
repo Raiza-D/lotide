e3078aa56a4f420239f23912326cd1f8b566d353
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-24 11:53:06 -0700

    Added alternative way to code, using ternary operator suggested by mentor. Also added comments for myself for when reviewing code in the future.

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,5 @@
-// FUNCTION IMPLEMENTATION HERE
-
-/* (Mod1,Wk1: assertEqual > Implement assertEqual):
-Original code with concatenated strings.
+/*(Mod1,Wk1: assertEqual > Implement assertEqual):
+Original code using concatenated strings.
 
 const assertEqual = function (actual, expected) {
   const pass = "";
@@ -26,10 +24,23 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE:
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1,1));
 // Testing scenarios of non-identical values:
-assertEqual("Weekend", "Weekend");
-assertEqual(1,5);
+console.log(assertEqual("Weekend", "Weekend"));
+console.log(assertEqual(1,5));
 
+/* Code Review notes from  mentor Konstantin Toroshchin:
+Suggestion to improve code further: use ternary operator.
+Warned against ternary operator. Code can become hard to read,
+so best to use if/else statements, like what I have.
 
+const assertEqual = function(actual, expected) {
+  const result = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` 
+      : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  return result;
+};
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1,1));
+console.log(assertEqual("Weekend", "Weekend"));
+console.log(assertEqual(1,5)); */
