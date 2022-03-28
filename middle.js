1613dee36c5672790a15d81c94c0645b7894879e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-27 18:28:04 -0700

    Ran eslint linter and fixed all errors.

--- middle.js
+++ middle.js
@@ -24,20 +24,19 @@ const assertArraysEqual = function(actual, expected) {
 const middle = function(array) {
   let middleArr = [];
 
-    if (array.length === 1 || array.length === 2) {
-      return middleArr;
-    }
-    if (array.length % 2 === 0) {
-      let y = array.length / 2;   // Contains the second middle element. Working with INDEX.
-      let x = y - 1  // Contains the first middle element, which is the one BEFORE y. Working with INDEX.
-      middleArr.push(array[x]); // Pushing ELEMENT indexed at x
-      middleArr.push(array[y]); // Pushing ELEMENT indexed at y
-    }
-    else {
-      let b = (array.length - 1) / 2 // Result is the array's INDEX.
-      middleArr.push(array[b]);  // Accessing array's ELEMENT then pushing to middleArr.
-    }
+  if (array.length === 1 || array.length === 2) {
     return middleArr;
+  }
+  if (array.length % 2 === 0) {
+    let y = array.length / 2;   // Contains the second middle element. Working with INDEX.
+    let x = y - 1;  // Contains the first middle element, which is the one BEFORE y. Working with INDEX.
+    middleArr.push(array[x]); // Pushing ELEMENT indexed at x
+    middleArr.push(array[y]); // Pushing ELEMENT indexed at y
+  } else {
+    let b = (array.length - 1) / 2; // Result is the array's INDEX.
+    middleArr.push(array[b]);  // Accessing array's ELEMENT then pushing to middleArr.
+  }
+  return middleArr;
 };
 
 // Test-cases:
