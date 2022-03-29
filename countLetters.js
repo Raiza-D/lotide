4cbef18fdae3210669d51e4b9858a695c4a3398c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 20:33:40 -0700

    Added for..of loop, if statement and nested if/else statement. Trial to see if this works.

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,5 @@
 // Function to test our assertions
-const assertEqual = function (actual, expected) {
+/*const assertEqual = function (actual, expected) {
   const pass = "✅✅✅ Assertion Passed: ";
   const fail = "🛑🛑🛑 Assertion Failed: ";
   if (actual === expected) {
@@ -7,8 +7,21 @@ const assertEqual = function (actual, expected) {
   } else {
     console.log(`${fail}` + actual + " !== " + expected);
   }
+}; */
+
+const countLetters = function(sentence) {
+  let letterCount = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+   if (sentence[i]) {
+     if (letterCount[i]) {
+       letterCount++;
+     } else {
+       letterCount[i] = 1;
+     }
+   }
+  }
+  return letterCount;
 };
 
-const countLetters = function() {
-
-};
+console.log(countLetters("Hollywood"));
