86ee595a2a21356dc25da15ae4ae51511e52ac9f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 19:58:28 -0700

    Removed blocks of comments to clean up file. Notes saved in earlier version of this file within repo.

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,3 @@
-/*(Mod1,Wk1: assertEqual > Implement assertEqual):
-Original code using concatenated strings.
-
-const assertEqual = function (actual, expected) {
-  const pass = "";
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-  }
-};*/
-
 /* (Mod1,Wk1: assertEqual > Improve assertEqual):
 Updated code that uses template literals. */
 
@@ -30,17 +18,3 @@ assertEqual(1,1);
 assertEqual("Weekend", "Weekend");
 assertEqual(1,5);
 
-/* Code Review notes from  mentor Konstantin Toroshchin:
-Suggestion to improve code further: use ternary operator.
-Warned against ternary operator. Code can become hard to read,
-so best to use if/else statements, like what I have.
-
-const assertEqual = function(actual, expected) {
-  const result = actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` 
-      : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
-  return result;
-};
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1,1));
-console.log(assertEqual("Weekend", "Weekend"));
-console.log(assertEqual(1,5)); */
