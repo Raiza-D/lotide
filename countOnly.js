f4f09b0480c1c06a8d7118cdddbb71060253eb04
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 19:26:57 -0700

    Went through iterations with mentor and added comments within for loop and assertion lines for reference. Will clean up later.

--- countOnly.js
+++ countOnly.js
@@ -12,15 +12,26 @@ const countOnly = function(allItems, itemsToCount) {
   let results = {};
     
   for (const item of allItems) {
+    // item is Karl. itemsToCount[Karl] ---> Look into itemsToCount object and find Karl. This should be false bc Karl is not in the itemsToCount.
+      // Karl is not stored anywhere. If you include Karl in test-code, you will get undefined === undefined.
+    // item is Salima. itemsToCount[Salima]. This should be false bc Salima is not in the itemsToCount.
+    // item is Agouhanna. itemsToCount[Agouhanna]. Agouhanna's value in the itemsToCount object false.
+      //Her name not saved in results object above. So undefined.
+    // item is Fang. itemsToCount[Fang], should be true.
+    // item is Kavith. itemsToCount[Kavith]. Does not exist in itemsToCount.
+    // item is Jason. itemsToCount[Jason].
     if (itemsToCount[item]) {
       if (results[item]) {
+        // results[Fang] is false. Fang is not yet found/inside the results object.
         results[item] += 1;
       } else {
         results[item] = 1;
+        // results = {Fang: 2, Jason: 1}
       }
     }
   }
-  return results;
+  console.log(results);
+  return results;  // Final results objects with all the names and their counts.
 };
 
 const firstNames = [
@@ -36,9 +47,18 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+// result1 = {Fang: 2, Jason: 1}
+// result1["Jason"] ---> actual. In result1, what is the count for Jason? Actual: 1. We expect 1. Terminal output: Assertion Passed.
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
+assertEqual(
+  result1["Jason"], 
+  1
+);
+
+assertEqual(result1["Karima"], undefined); // Why is actual value "undefined?"
+// Karima cannot be found in itemsToCount object.
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Agouhanna"], undefined); // Why is actual value "undefined?"
+// Agouhanna has a "false" value assigned within itemsToCount object.
 assertEqual(result1["Fang"], 3);
+assertEqual(result1["Kavith"], undefined);
