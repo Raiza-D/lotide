b0c3150b05f45270a626d0ad2b7d802f09fac59f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 19:49:49 -0700

    Removed prior blocks of commented notes. Entered comments where mostly necessary for my future reference.

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,4 @@
+// Function to test our assertions
 const assertEqual = function(actual, expected) {
   const pass = "✅✅✅ Assertion Passed: ";
   const fail = "🛑🛑🛑 Assertion Failed: ";
@@ -7,30 +8,20 @@ const assertEqual = function(actual, expected) {
     console.log(`${fail}` + actual + " !== " + expected);
   }
 };
-
+// Function that takes in collection of items and returns the counts
+// only for specific subset of those items.
 const countOnly = function(allItems, itemsToCount) {
-  let results = {};
+  let results = {};  // Save the items and their count here
     
-  for (const item of allItems) {
-    // item is Karl. itemsToCount[Karl] ---> Look into itemsToCount object and find Karl. This should be false bc Karl is not in the itemsToCount.
-      // Karl is not stored anywhere. If you include Karl in test-code, you will get undefined === undefined.
-    // item is Salima. itemsToCount[Salima]. This should be false bc Salima is not in the itemsToCount.
-    // item is Agouhanna. itemsToCount[Agouhanna]. Agouhanna's value in the itemsToCount object false.
-      //Her name not saved in results object above. So undefined.
-    // item is Fang. itemsToCount[Fang], should be true.
-    // item is Kavith. itemsToCount[Kavith]. Does not exist in itemsToCount.
-    // item is Jason. itemsToCount[Jason].
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        // results[Fang] is false. Fang is not yet found/inside the results object.
+  for (const item of allItems) { // Loop through each element in given array.
+    if (itemsToCount[item]) {  // If item can be found in itemsToCount object
+      if (results[item]) {  // If the same item is found in the results object
         results[item] += 1;
       } else {
         results[item] = 1;
-        // results = {Fang: 2, Jason: 1}
       }
     }
   }
-  console.log(results);
   return results;  // Final results objects with all the names and their counts.
 };
 
@@ -47,18 +38,10 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
-// result1 = {Fang: 2, Jason: 1}
-// result1["Jason"] ---> actual. In result1, what is the count for Jason? Actual: 1. We expect 1. Terminal output: Assertion Passed.
 
-assertEqual(
-  result1["Jason"], 
-  1
-);
-
-assertEqual(result1["Karima"], undefined); // Why is actual value "undefined?"
-// Karima cannot be found in itemsToCount object.
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined); // Actual = "undefined". Karima is not saved in results.
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined); // Why is actual value "undefined?"
-// Agouhanna has a "false" value assigned within itemsToCount object.
+assertEqual(result1["Agouhanna"], undefined);
 assertEqual(result1["Fang"], 3);
 assertEqual(result1["Kavith"], undefined);
