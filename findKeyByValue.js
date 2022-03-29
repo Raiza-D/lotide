a079fbcb077827810f2e16cb0c8d9232e3117513
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-29 16:23:17 -0700

    Corrected syntax on Line 14 by using bracket notation. Corrected console.log position to AFTER declaration of new object. Ran linter and fixed errors.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -11,15 +11,12 @@ const assertEqual = function(actual, expected) {
 const findKeyByValue = function(object, value) {
 
   for (const key in object) {  // key represents ONLY the object's key. NOT including its value.
-    if (object.key === value) { // Use bracket notation. key is dynamic here (changing w/ each iteration)
+    if (object[key] === value) { // Use bracket notation. key is dynamic here (changing w/ each iteration)
       return key;
     }
   }
   return undefined;
 };
-
-// Tests to ensure findKeyByValue function output correct:
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
 
 const bestTVShowsByGenre = {
   sciFi: "The Expanse",
@@ -27,23 +24,9 @@ const bestTVShowsByGenre = {
   drama: "The Wire"
 };
 
+// Tests to ensure findKeyByValue function output correct:
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
 // Examples testing our assertions:
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
-
-/* Method below makes use of Object.keys(). Keys are stored in an array.
-const findKeyByValue = function (object, value) {
-  keyArray = Object.keys(object);
-
-  for (const arrElement of keyArray) {
-    if (object[arrElement] === value) {
-      return arrElement;
-    }
-  }
-  return undefined;
-}; */
-
-/* Test-codes to demonstrate why we cannot use dot notation in Line 14:
-console.log(bestTVShowsByGenre.comedy);
-console.log(bestTVShowsByGenre[comedy]);
-console.log(bestTVShowsByGenre["comedy"]); */
