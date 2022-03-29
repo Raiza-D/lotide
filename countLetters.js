a7e422a4e8c6cae469d53fb6dcfcb2b2b1df770a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 21:40:55 -0700

    Added resultsContent object to house actual results of results from countLetters. Then added test assertions using assertEqual function for testing. assertEqual function uses resultsCotent as an argument. Check with mentor if test assertion code lines correct.

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,5 @@
 // Function to test our assertions
-/*const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   const pass = "✅✅✅ Assertion Passed: ";
   const fail = "🛑🛑🛑 Assertion Failed: ";
   if (actual === expected) {
@@ -7,21 +7,29 @@
   } else {
     console.log(`${fail}` + actual + " !== " + expected);
   }
-}; */
-
-const countLetters = function(sentence) {
-  let letterCount;
-
-  for (let i = 0; i < sentence.length; i++) {
-   if (sentence) {
-     if (letterCount) {
-       letterCount += 1;
-     } else {
-       letterCount = 1;
-     }
-   }
-  }
-  return letterCount;
 };
 
-console.log(countLetters("Hollywood"));
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) { // Loop through each letter
+    if (!results[letter]) {  // If that letter is NOT found in the results object
+      results[letter] = 0;  // Then add the letter to results and assign value of zero
+    }
+    results[letter]++;  // Letter already in results. Add 1 each time letter appears in iteration
+  }
+  return results;
+};
+
+// Test-codes to make sure correct data saved in results object's key-value pairs.
+// console.log(countLetters("Hollywood"));
+// console.log(countLetters("Mountain"));
+
+const resultsContent = countLetters("Hollywood");
+
+// Test assertions
+assertEqual(resultsContent["o"], 3);
+assertEqual(resultsContent["h"], 0); // Lowercase "h" does not exist, therefore undefined.
+
+
+
