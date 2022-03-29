dd4f27625c29d17590c9053884d64bc32e93cb07
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-28 22:12:26 -0700

    Copy/pasted assertArraysEqual function and eqArrays into file. Then declared letterPositions function.

---
+++ letterPositions.js
@@ -0,0 +1,28 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  const arrayPass = "✅ Assertion passed. ";
+  const arrayFail = "❌ Assertion failed. ";
+
+  if (eqArrays(actual, expected)) {
+    console.log(arrayPass + actual + " === " + expected);
+  } else {
+    console.log(arrayFail + actual + " !== " + expected);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // code to update results here
+  return results;
+};
