ae9224bec4a9cd990efd0c0878b2d47e6d8f69c6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 13:30:59 -0700

    Declared a words array.Added map function definition and simple test code below it. Called map function to test output at different steps.

---
+++ map.js
@@ -0,0 +1,10 @@
+// Create our own version of map method
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function() {
+  const results = [];
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
