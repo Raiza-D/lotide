39e5fe07aa922bf9057224b74c374e4fab4f5895
Raiza De Guzman <santos_dg7@hotmail.com>
2022-03-31 17:20:27 -0700

    Added additional comments.

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,6 @@
-// Function to test our assertions
-const assertEqual = function(actual, expected) {
+// assertEqual function to compare Boolean values true/false.
+// Compares actual result against what we expect the result to be.
+const assertEqual = function (actual, expected) {
   const pass = "✅✅✅ Assertion Passed: ";
   const fail = "🛑🛑🛑 Assertion Failed: ";
   if (actual === expected) {
@@ -10,12 +11,15 @@ const assertEqual = function(actual, expected) {
 };
 // Function that takes in collection of items and returns the counts
 // only for specific subset of those items.
-const countOnly = function(allItems, itemsToCount) {
-  const results = {};  // Save the items and their count here
-    
-  for (const item of allItems) { // Loop through each element in given array.
-    if (itemsToCount[item]) {  // If item can be found in itemsToCount object
-      if (results[item]) {  // If the same item is found in the results object
+const countOnly = function (allItems, itemsToCount) {
+  const results = {}; // Save the items and their count here
+
+  for (const item of allItems) {
+    // Loop through each element in given array.
+    if (itemsToCount[item]) {
+      // If item can be found in itemsToCount object AND its value is TRUE
+      if (results[item]) {
+        // If the same item is found in the results object
         results[item] += 1;
       } else {
         results[item] = 1;
@@ -34,11 +38,16 @@ const firstNames = [
   "Jason",
   "Salima",
   "Fang",
-  "Joe"
+  "Joe",
 ];
 
-const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
-// result1 = results object from countOnly function.
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+// result1 = results object from countOnly function stored in this variable.
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined); // Actual = "undefined". Karima is not saved in results.
