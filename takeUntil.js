b6ead1b10ac00bb66f30b56074fc119d783b6349
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 16:47:20 -0700

    Copy/pasted eqArrays and assertArraysEqual functions into file. These will be used to compare results against our assertions. Also added code for test-scenarios as seen on Compass.

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,54 @@
+// Compares two arrays by comparing its length then comparing each value
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Function to test if assertions regarding the two arrays are correct.
+// Makes use of eqArrays.
+const assertArraysEqual = function(actual, expected) {
+  const arrayPass = "✅ Assertion passed. ";
+  const arrayFail = "❌ Assertion failed. ";
+
+  if (eqArrays(actual, expected)) {
+    console.log(arrayPass + actual + " === " + expected);
+  } else {
+    console.log(arrayFail + actual + " !== " + expected);
+  }
+};
+
 // Definition & implementation of takeUntil function
 const takeUntil = function(array, callback) {
+
   
 };
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, x => x === ",");
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
