dc6f416731acd51135feb8e62b457cb7f0fb9c14
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 13:46:17 -0700

    Completed logic for map function and tested outputs. Copy/pasted eqArrays function code and assertArraysEqual function code into the file. These two functions will be used to test our map function.

--- map.js
+++ map.js
@@ -1,10 +1,37 @@
-// Create our own version of map method
+// Compares two arrays by comparing its length then comparing each value
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  const arrayPass = "✅ Assertion passed. ";
+  const arrayFail = "❌ Assertion failed. ";
+
+  if (eqArrays(actual, expected)) {
+    console.log(arrayPass + actual + " === " + expected);
+  } else {
+    console.log(arrayFail + actual + " !== " + expected);
+  }
+};
+
+// Definition and implementation of our own version of map method
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
+const map = function (array, callback) {
   const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
   return results;
-}
+};
 
-const results1 = map(words, word => word[0]);
+const results1 = map(words, (word) => word[0]);
 console.log(results1);
