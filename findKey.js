ac1b60b14243c72dc2b7e6fa6ac5fa07c6d00428
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 17:34:05 -0700

    Corrected code logic as prior one was not working. The current one in this file now works. Output on Terminal looks correct. Need to add a few more tests to make sure findKey function working correctly.

--- findKey.js
+++ findKey.js
@@ -13,18 +13,21 @@ Function will scan the object and return the first key for
 which callback returns a truthy value. If no key is found, return "undefined". */
 const findKey = function(object, callback) {
   for (const key in object) {
-    if (object[key] !== true) {
-      return false;
+    if (callback(object[key]) === true) {
+      return key;
     }
   }
-  return callback(value);
+  return undefined;
 };
 
-findKey({
+
+const results = findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(results, "noma");
