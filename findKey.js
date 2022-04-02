b18f1a0438a5191f4e342a477c7754570a486181
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 19:11:42 -0700

    Revised comments. Ran linter and fixed errors. Ran file again and everything looks correct.

--- findKey.js
+++ findKey.js
@@ -22,7 +22,8 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-/* Store result from running findKey function. So can be used as an
+/* Test-scenario one
+Stores result from running findKey function, into a variable. So can be used as an
 argument when using assertEqual function. Result will be key name. */
 const resultsOne = findKey({
   "Blue Hill": { stars: 1 },
@@ -31,28 +32,32 @@ const resultsOne = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma". Wrap this in a console.log to make sure result correct.
+}, x => x.stars === 2); // => "noma". Wrap this in a console.log to see if result correct.
 
-/*Store result from running findKey function. So can be used as an
+
+/* Test-scenario two
+Stores result from running findKey function, into a variable. So can be used as an
 argument when using assertEqual function. Result will be key name. */
-const resultsTwo = findKey({
-    London: { rating: 9 },
-    Paris: { rating: 8 },
-    Vancouver: { rating: 10 },
-    Berlin: { rating: 7 },
-    Brighton: { rating: 8 },
-  },
-  x => x.rating === 9
-); // => London. Wrap this in a console.log to make sure result correct.
+const resultsTwo = findKey({ // Value of each key here is a nested object.
+  London: { rating: 9 },
+  Paris: { rating: 8 },
+  Vancouver: { rating: 10 },
+  Berlin: { rating: 7 },
+  Brighton: { rating: 8 },
+}, x => x.rating === 9); // => London. Wrap this in a console.log to see if result correct.
+  // x here represents second argument: callback.
+  // x.rating represents key in iteration and its value.
 
+
+/* Test-scenario three - undefined and nested object value's are a string.
+Stores result from running findKey function, into a variable. So can be used as an
+argument when using assertEqual function. Result will be key name. */
 const resultsThree = findKey({
-    Frosty: { level: "Novice" },
-    Everest: { level: "Expert" },
-    Rainier: { level: "Intermediate" },
-    Denali: { level: "Expert" }
-  },
-  x => x.level === "Easy" // Will return undefined. No key found that has value of "Easy"
-); // => Everest
+  Frosty: { level: "Novice" },
+  Everest: { level: "Expert" },
+  Rainier: { level: "Intermediate" },
+  Denali: { level: "Expert" }
+}, x => x.level === "Easy"); // => undefined. No key found that has value of "Easy"
 
 assertEqual(resultsOne, "noma");
 assertEqual(resultsTwo, "London");
