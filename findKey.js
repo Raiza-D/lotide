837b782e573298deb85a8d721fcdbc77451cd2da
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-01 17:17:02 -0700

    Copy/pasted assertEqual function into file. Defined findKey function. Entered test-case.

---
+++ findKey.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  const pass = "✅✅✅ Assertion Passed: ";
+  const fail = "🛑🛑🛑 Assertion Failed: ";
+  if (actual === expected) {
+    console.log(`${pass}` + actual + " === " + expected);
+  } else {
+    console.log(`${fail}` + actual + " !== " + expected);
+  }
+};
+
+const findKey = function() {
+
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
