7cdf2854fad54b3ab2318f23f8b6f283fba9d5e4
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-12 13:41:10 -0700

    Created test subdirectory. Created assertEqualTest.js and headTest.js files. Each file contains test code for both functions. Both files require use of the respective functions.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual");
+
+// TEST CODE for assertEqual function within assertEqual.js
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Weekend", "Weekend");
+assertEqual(1, 5);
+
---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require("../assertEqual");
+const head = require("../head");
+
+// TEST CODE for head function, which makes use of assertEqual function.
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([9]), 9); //Test-case an array with only one element. Should still yield that one element as its head.
+assertEqual(head([]), 25); //Test-case an empty array should yield "undefined" as its head.
+assertEqual(head([-20, 9, -99]), 98);
