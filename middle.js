cde2ab42c41580162a88e2ea809c29407e52af7a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-12 15:43:58 -0700

    Corrected my error in prior push. Didn't need to import eqArrays function into middle.js. So deleted that. Also removed assertArraysEqual function.

--- middle.js
+++ middle.js
@@ -1,16 +1,4 @@
-const eqArrays = require("./eqArrays");
 
-
-const assertArraysEqual = function(actual, expected) {
-  const arrayPass = "✅ Assertion passed. ";
-  const arrayFail = "❌ Assertion failed. ";
-
-  if (eqArrays(actual, expected)) {
-    console.log(arrayPass + actual + " === " + expected);
-  } else {
-    console.log(arrayFail + actual + " !== " + expected);
-  }
-};
 
 const middle = function(array) {
   let middleArr = [];
