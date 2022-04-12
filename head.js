651a7f7db1165612631070ba46f791e9fb836392
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-12 13:46:38 -0700

    Removed Test Codes by copy/pasting them into headTest test file within test sub-directory. Added module.exports code at the bottom of file to export the head function into the test file.

--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  const pass = "✅✅✅ Assertion Passed: ";
-  const fail = "🛑🛑🛑 Assertion Failed: ";
-  if (actual === expected) {
-    console.log(`${pass}` + actual + " === " + expected);
-  } else {
-    console.log(`${fail}` + actual + " !== " + expected);
-  }
-};
+// const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([9]), 9); //Test-case an array with only one element. Should still yield that one element as its head.
-assertEqual(head([]), 25); //Test-case an empty array should yield "undefined" as its head.
-assertEqual(head([-20, 9, -99]), 98);
+module.exports = head;
