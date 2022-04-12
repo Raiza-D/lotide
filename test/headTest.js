f717745a1215da44191849487faa4caac9321b30
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-12 13:56:19 -0700

    Added test code just for head function, without making use of assertion function assertEqual. The console.log test codes added above assertion test codes.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,11 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-// TEST CODE for head function, which makes use of assertEqual function.
+// TEST CODE for head function
+console.log(head([1, 2, 3, 4]));
+console.log(head(["Everest", "Waddington", "Annapurna"]));
+
+// TEST CODE for head function, which makes use of assertEqual function. Checking assertions.
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([9]), 9); //Test-case an array with only one element. Should still yield that one element as its head.
