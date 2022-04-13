e78b36dc9c24c2a6d9226ef9138eaaca470f7518
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 12:44:34 -0700

    Removed old commented test codes below file. Those tests made use of assertEqual function. Ran eslint linter, no errors.

--- test/middleTest.js
+++ test/middleTest.js
@@ -16,10 +16,3 @@ describe("#middle", () => {
 
 });
 
-// Test-cases for middle function to ensure working as it should:
-// console.log(middle([1, 2])); // Should return an empty array
-// console.log(middle([1, 2, 3])); // Should return the middle element of 2
-// console.log(middle([1, 2, 3, 4])); // Should return the two middle elements of 2 & 3
-
-//Testing assertions for middle function:
-// assertArraysEqual(middle([1, 2, 3]), [2]);
