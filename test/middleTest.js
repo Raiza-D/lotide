6831d3cf4f620010cecede83e7985a2413fe2ba7
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 12:35:00 -0700

    Updated require code on Line 1 to require chai assertion library. Wrote describe function and first it function. The first it test is to test scenario of an array with only two elements. Should return an empty array. Output accurate and passing.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,17 @@
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-const assertArraysEqual = require("../assertArraysEqual");
+describe("#middle", () => {
+  it("returns an empty array [] for an array with only two elements [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  
+});
 
 // Test-cases for middle function to ensure working as it should:
-console.log(middle([1, 2])); // Should return an empty array
-console.log(middle([1, 2, 3])); // Should return the middle element of 2
-console.log(middle([1, 2, 3, 4])); // Should return the two middle elements of 2 & 3
+// console.log(middle([1, 2])); // Should return an empty array
+// console.log(middle([1, 2, 3])); // Should return the middle element of 2
+// console.log(middle([1, 2, 3, 4])); // Should return the two middle elements of 2 & 3
 
 //Testing assertions for middle function:
-assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
