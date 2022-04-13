6f1a17929da228b4c9220b86d57eb93f48762463
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 15:38:34 -0700

    Added module exports code to export findKey function. Commented out all test codes.

--- findKey.js
+++ findKey.js
@@ -25,26 +25,26 @@ const findKey = function(object, callback) {
 /* Test-scenario one
 Stores result from running findKey function, into a variable. So can be used as an
 argument when using assertEqual function. Result will be key name. */
-const resultsOne = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma". Wrap this in a console.log to see if result correct.
+// const resultsOne = findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2); // => "noma". Wrap this in a console.log to see if result correct.
 
 
 /* Test-scenario two
 Stores result from running findKey function, into a variable. So can be used as an
 argument when using assertEqual function. Result will be key name. */
-const resultsTwo = findKey({ // Value of each key here is a nested object.
-  London: { rating: 9 },
-  Paris: { rating: 8 },
-  Vancouver: { rating: 10 },
-  Berlin: { rating: 7 },
-  Brighton: { rating: 8 },
-}, x => x.rating === 9); // => London. Wrap this in a console.log to see if result correct.
+// const resultsTwo = findKey({ // Value of each key here is a nested object.
+//   London: { rating: 9 },
+//   Paris: { rating: 8 },
+//   Vancouver: { rating: 10 },
+//   Berlin: { rating: 7 },
+//   Brighton: { rating: 8 },
+// }, x => x.rating === 9); // => London. Wrap this in a console.log to see if result correct.
   // x here represents second argument: callback.
   // x.rating represents key in iteration and its value.
 
@@ -52,13 +52,15 @@ const resultsTwo = findKey({ // Value of each key here is a nested object.
 /* Test-scenario three - undefined and nested object value's are a string.
 Stores result from running findKey function, into a variable. So can be used as an
 argument when using assertEqual function. Result will be key name. */
-const resultsThree = findKey({
-  Frosty: { level: "Novice" },
-  Everest: { level: "Expert" },
-  Rainier: { level: "Intermediate" },
-  Denali: { level: "Expert" }
-}, x => x.level === "Easy"); // => undefined. No key found that has value of "Easy"
+// const resultsThree = findKey({
+//   Frosty: { level: "Novice" },
+//   Everest: { level: "Expert" },
+//   Rainier: { level: "Intermediate" },
+//   Denali: { level: "Expert" }
+// }, x => x.level === "Easy"); // => undefined. No key found that has value of "Easy"
 
-assertEqual(resultsOne, "noma");
-assertEqual(resultsTwo, "London");
-assertEqual(resultsThree, undefined);
+// assertEqual(resultsOne, "noma");
+// assertEqual(resultsTwo, "London");
+// assertEqual(resultsThree, undefined);
+
+module.exports = findKey;
