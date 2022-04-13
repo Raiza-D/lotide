4f6a45da35f5b37d3a66310060e7cf1d5ea8ba13
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 12:01:35 -0700

    Deleted old commented non-mocha tests written on bottom of file. No longer needed. Ran mocha tests to ensure output correct and passing.

--- test/headTest.js
+++ test/headTest.js
@@ -19,15 +19,3 @@ describe("#head", () => {
   });
 
 });
-
-
-// TEST CODE for head function
-// console.log(head([1, 2, 3, 4]));
-// console.log(head(["Everest", "Waddington", "Annapurna"]));
-
-// TEST CODE for head function, which makes use of assertEqual function. Checking assertions.
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([9]), 9); //Test-case an array with only one element. Should still yield that one element as its head.
-// assertEqual(head([]), 25); //Test-case an empty array should yield "undefined" as its head.
-// assertEqual(head([-20, 9, -99]), 98);
