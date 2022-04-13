6dd37f66081e766bf7a39eab9ad9c97aefc47e95
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 13:18:17 -0700

    Added the rest of utility functions into file.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+const flatten = require("./flatten");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+
 
 module.exports = {
   head: head,
   tail: tail,
-  middle: middle
+  middle: middle,
+  takeUntil: takeUntil,
+  without: without,
+  flatten: flatten,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  letterPositions: letterPositions,
+  map: map,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue
 };
