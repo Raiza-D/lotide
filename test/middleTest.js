8b31895bd087c7db9af89e9dd872b41b1b413059
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-13 12:41:52 -0700

    Added two more it tests. One testing an array with an odd number of elements, should return the middle element. The other scenario within the it test is for an array with an even number of elements. Should return the two middle elements.

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,7 +5,15 @@ describe("#middle", () => {
   it("returns an empty array [] for an array with only two elements [1, 2]", () => {
     assert.deepEqual(middle([1, 2]), []);
   });
-  
+
+  it("returns the middle element of [ 2 ] for an array with an odd number of elements [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns the two middle elements [ 2, 3 ] for an array with an even number of elements [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
 });
 
 // Test-cases for middle function to ensure working as it should:
